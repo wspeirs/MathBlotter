5d5e773fa49b59ac598768a5b49f32aa23fdcc51
William R. Speirs <bill.speirs@gmail.com>
2012-03-10 16:25:56 -0500

    - Added a MathNode for all AST nodes to extend - Added logging packages

---
+++ src/main/java/com/educatedsolutions/parser/javacc/MathNode.java
@@ -0,0 +1,13 @@
+package com.educatedsolutions.parser.javacc;
+
+public class MathNode {
+    private String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
